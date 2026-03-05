8172fe6ff481d13cca8be64e8130889975428697
Amazi <abayshemelisshiferaw@gmail.com>
2026-03-05 17:31:48 +0300

    new commit

---
+++ passanger/passenger.js
@@ -0,0 +1,54 @@
+/*console.log("hello my Niga")
+bonesBoint = 50;
+console.log(bonesBoint)
+bonesBoint = bonesBoint+50;
+console.log(bonesBoint)
+bonesBoint= bonesBoint-75;
+console.log(bonesBoint)
+bonesBoint = bonesBoint +45
+console.log(bonesBoint)
+
+*/
+
+
+let CouselEl = document.getElementById("count_el").innerText =5;
+console.log(CounselEl)
+
+let count =0;
+function Increament(){
+    count= count+1;
+    console.log( "Buttoen is clicked");
+    
+
+}
+Increament()
+
+
+/*
+let lap1 =43;
+let lap2 =34;
+let lap3 =50;
+function countDown(){
+    console.log(42)
+
+let sum = lap1+lap2+lap3;
+console.log(sum)
+
+}
+countDown();
+
+let lapsCompleted =0;
+function Increaments(){
+    
+    lapsCompleted = lapsCompleted + 1;
+    console.log(lapsCompleted)
+}
+Increaments();
+Increaments();
+Increaments();
+Increaments();
+*/
+
+
+
+
