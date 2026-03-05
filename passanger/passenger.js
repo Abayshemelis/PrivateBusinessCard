035a54fb29624c040f984e58c9deca44e1143b83
Amazi <abayshemelisshiferaw@gmail.com>
2026-03-05 21:11:17 +0300

    new commit

--- passanger/passenger.js
+++ passanger/passenger.js
@@ -10,19 +10,26 @@ console.log(bonesBoint)
 
 */
 
+// let CountEl = document.getElementById("count_el")
+// console.log(CountEl)
 
-let CouselEl = document.getElementById("count_el").innerText =5;
-console.log(CounselEl)
+// let count = 0;
+// function Increament() {
+//     count = count + 1;
+//     CountEl.innerText = count
+// }
+// Increament()
 
-let count =0;
-function Increament(){
-    count= count+1;
-    console.log( "Buttoen is clicked");
-    
+// function save() {
+//     console.log(count)
 
-}
-Increament()
+// }
+// save()
 
+const prompt = require("prompt-sync")()
+
+let name = prompt("Whats your name: ")
+console.log(name)
 
 /*
 let lap1 =43;
@@ -39,7 +46,7 @@ countDown();
 
 let lapsCompleted =0;
 function Increaments(){
-    
+
     lapsCompleted = lapsCompleted + 1;
     console.log(lapsCompleted)
 }
@@ -48,6 +55,26 @@ Increaments();
 Increaments();
 Increaments();
 */
+//let username = "Abay"
+//let message = "You have tree new notification "
+//let messageToUser = message + " ," + username +"!"
+//console.log(messageToUser)
+
+// let name = "Abay"
+// let greeting = " Hi my name is"
+// let mygreeting = greeting + "," + name + "!"
+// console.log(mygreeting)
+
+// let myName = "abaytu"
+// let Greeting = " welcome Back "
+// let MyGreeting = Greeting + " ," + myName + "!"
+// console.log(MyGreeting)
+// let welcomeEl = document.getElementById("welcome_el")
+
+// welcomeEl.innerText = MyGreeting
+// welcomeEl.innerText += " 👌"
+
+
 
 
 
