16df74d829e5fc884ea03b3365d71abc4372ae47
Amazi <abayshemelisshiferaw@gmail.com>
2026-03-06 20:25:12 +0300

    new commit

--- passanger/passenger.js
+++ passanger/passenger.js
@@ -1,79 +1,23 @@
-/*console.log("hello my Niga")
-bonesBoint = 50;
-console.log(bonesBoint)
-bonesBoint = bonesBoint+50;
-console.log(bonesBoint)
-bonesBoint= bonesBoint-75;
-console.log(bonesBoint)
-bonesBoint = bonesBoint +45
-console.log(bonesBoint)
 
-*/
+let saveEl = document.getElementById("save_el")
+let CountEl = document.getElementById("count_el")
+let count = 0;
 
-// let CountEl = document.getElementById("count_el")
-// console.log(CountEl)
 
-// let count = 0;
-// function Increament() {
-//     count = count + 1;
-//     CountEl.innerText = count
-// }
-// Increament()
+function Increament() {
+    count += 1;
+    CountEl.textContent = count
+} 
 
-// function save() {
-//     console.log(count)
+function save() {
+    let countstr = count + " - "
+    saveEl.innerText = countstr
 
-// }
-// save()
 
-const prompt = require("prompt-sync")()
-
-let name = prompt("Whats your name: ")
-console.log(name)
-
-/*
-let lap1 =43;
-let lap2 =34;
-let lap3 =50;
-function countDown(){
-    console.log(42)
-
-let sum = lap1+lap2+lap3;
-console.log(sum)
-
+   CountEl.textContent = 0
+    console.log(count)
 }
-countDown();
-
-let lapsCompleted =0;
-function Increaments(){
-
-    lapsCompleted = lapsCompleted + 1;
-    console.log(lapsCompleted)
-}
-Increaments();
-Increaments();
-Increaments();
-Increaments();
-*/
-//let username = "Abay"
-//let message = "You have tree new notification "
-//let messageToUser = message + " ," + username +"!"
-//console.log(messageToUser)
-
-// let name = "Abay"
-// let greeting = " Hi my name is"
-// let mygreeting = greeting + "," + name + "!"
-// console.log(mygreeting)
-
-// let myName = "abaytu"
-// let Greeting = " welcome Back "
-// let MyGreeting = Greeting + " ," + myName + "!"
-// console.log(MyGreeting)
-// let welcomeEl = document.getElementById("welcome_el")
-
-// welcomeEl.innerText = MyGreeting
-// welcomeEl.innerText += " 👌"
-
+ 
 
 
 
---
+++ passanger/practice.js
@@ -0,0 +1,7 @@
+let welcomeEl = document.getElementById("Welcome_el")
+let name = "Abay" 
+let Greeting =" Welcome  Back "
+let myGreeting = Greeting +","+ name+"!"
+console.log(myGreeting)
+welcomeEl.innerText = myGreeting
+welcomeEl.innerText+='🤗'
